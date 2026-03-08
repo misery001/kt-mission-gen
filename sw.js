3594ce7b7a9e9c553d08a0bc14b7efbc80d3302e
misery001 <44144631+misery001@users.noreply.github.com>
2026-03-08 11:29:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kt-cogitator-v3';
+const CACHE_NAME = 'kt-cogitator-v5';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -35,7 +35,9 @@ const ASSETS_TO_CACHE = [
   './maps/gallowdark_13.jpg'
 ];
 
+// Install Event: Forces immediate activation
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
@@ -43,6 +45,23 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// Activate Event: Seeks and destroys old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Purging old cogitator data:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event: Serves files from the cache if offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
